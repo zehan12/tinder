38d62db36bbd0fcf856c299d258867e8ec1d9cc7
Zehan Khan <zehan9211@gmail.com>
2023-02-02 19:51:06 +0530

    start dating

--- src/App.js
+++ src/App.js
@@ -1,42 +1,71 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
 import TopBar from "./components/TopBar"
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import SwipeableImage from './components/SwipeableImage';
 import axios from 'axios';
+import BottomBar from './components/BottomBar';
+import Swipes from './components/Swipes';
+import usersData from "./data/usersData.json"
 
 export default function App() {
 
   const [users, setUsers] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0)
+  const swipesRef = useRef(null)
 
-  async function fetchUsers() {
-    try {
-      const { data } = await axios.get('https://randomuser.me/api/?gender=female&results=50')
-      setUsers(data.results)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  console.log(users[0],"why")
 
   useEffect(() => {
-    fetchUsers()
+    setUsers(usersData.users)
   }, [])
+
+  function handleLike() {
+    console.log('like')
+    nextUser()
+  }
+
+  function handlePass() {
+    console.log('pass')
+    nextUser()
+  }
+
+  function nextUser() {
+    const nextIndex = users.length - 2 === currentIndex ? 0 : currentIndex + 1
+    setCurrentIndex(nextIndex)
+  }
+
+  function handleLikePress() {
+    swipesRef.current.openLeft()
+  }
+  function handlePassPress() {
+    swipesRef.current.openRight()
+  }
 
   return (
     <SafeAreaView style={styles.wrapper}>
-      {/* <View style={styles.container}> */}
       <TopBar />
       <View style={styles.swipes} >
-        {
+        {/* {
           users.length !== 0 &&
-            <SwipeableImage user={users[currentIndex]} />
-          
-        }
+          <SwipeableImage user={users[currentIndex]} />
+
+        } */}
+        {users.length > 1 &&
+          users.map(
+            (u, i) =>
+              currentIndex === i && (
+                <Swipes
+                  key={i}
+                  ref={swipesRef}
+                  currentIndex={currentIndex}
+                  users={users}
+                  handleLike={handleLike}
+                  handlePass={handlePass}
+                ></Swipes>
+              )
+          )}
       </View>
-      {/* </View> */}
+      <BottomBar />
     </SafeAreaView>
   );
 }
